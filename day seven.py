d8995c627f808733e5ab4e114b5bf4d135a23b3d
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-09 13:10:31 +0000

    commit

--- day seven.py
+++ day seven.py
@@ -65,6 +65,51 @@ def solution_part_one(input_path):
             total += find_dir_size(dir)
     return total
 
+def solution_part_two(input_path):
+    visited_dirs = {}
+    with open(input_path) as f:
+        lines = f.read()
+    lines = lines.split('$')
+    current_dir = 'home'
+    for line in lines:
+        line = line.split('\n')
+        command = line[0]
+        if ' cd ' in command:
+            new_dir = command.split()[-1]
+            if new_dir in visited_dirs.keys():
+                current_dir = current_dir + new_dir
+            elif new_dir == '..':
+                current_dir = visited_dirs[current_dir].parent
+            else:
+                visited_dirs[current_dir+'/'+new_dir] = directory(current_dir+'/'+new_dir, current_dir)
+                current_dir = current_dir+'/'+new_dir
+        elif command == ' ls':
+            items = line[1:]
+            for item in items:
+                if item != '':
+                    if 'dir ' in item:
+                        new_dir_name = item.split()[-1]
+                        visited_dirs[current_dir].subdirs.add(current_dir+'/'+new_dir_name)
+                    else:
+                        if item.split()[-1] not in visited_dirs[current_dir].files:
+                            visited_dirs[current_dir].files[item.split()[-1]] = (file(item.split()[-1], int(item.split()[0])))
 
+    """dfs size finding function"""
+    def find_dir_size(dir):
+        size = 0
+        for file in visited_dirs[dir].files:
+            size += visited_dirs[dir].files[file].size
+        for subdir in visited_dirs[dir].subdirs:
+            size += find_dir_size(subdir)
+        return size
 
-print(solution_part_one('input.txt'))
+    smallest_poss = 100000000000
+    target = 70000000-find_dir_size('home//')
+    target = (30000000-target)
+    for dir in visited_dirs.keys():
+        if find_dir_size(dir) > target:
+            if find_dir_size(dir) < smallest_poss:
+                smallest_poss = find_dir_size(dir)
+    return smallest_poss
+
+print(solution_part_two('input.txt'))
