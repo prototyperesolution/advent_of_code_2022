17e6fee7dea7aa53a0df0ebd4af98ea29ac460a1
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-13 21:41:58 +0000

    commit

---
+++ day_ten.py
@@ -0,0 +1,26 @@
+import os
+import re
+os.chdir('D:/adventofcode2022/day ten')
+import numpy as np
+import cv2
+
+def solution_part_one(input_path):
+    with open(input_path) as f:
+        lines = f.read()
+    instructions = lines.split('\n')[:-1]
+    cycles = 0
+    sigstrength = 1
+    cts = {}
+    for instr in instructions:
+        if 'noop' in instr:
+            cycles += 1
+            cts[cycles] = sigstrength
+        else:
+            for i in range(2):
+                cycles += 1
+                cts[cycles] = sigstrength
+            sigstrength += int(instr.split()[-1])
+
+    ans = sum([x*cts[x] for x in range(20,260,40)])
+    return ans
+solution_part_one('input.txt')
