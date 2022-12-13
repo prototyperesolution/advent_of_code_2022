ad475f399eb3080f7794329f392eff02cd5befaa
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-13 21:22:30 +0000

    commit

--- day_nine.py
+++ day_nine.py
@@ -8,38 +8,50 @@ def solution_part_one(input_path):
     with open(input_path) as f:
         lines = f.read()
     lines = lines.split('\n')[:-1]
-    h_pos, t_pos = np.array([0,0]),np.array([0,0])
+    h_pos, t_pos = [0,0],[0,0]
     t_set = set()
-    t_list = []
+    md = {'U':[0,1],'D':[0,-1],'R':[1,0],'L':[-1,0]}
     for line in lines:
         dir, dis = line.split()
         for i in range(int(dis)):
-            if dir == 'U':
-                h_pos[1] += 1
-            elif dir == 'R':
-                h_pos[0] += 1
-            elif dir == 'L':
-                h_pos[0] -=1
-            else:
-                h_pos[1] -= 1
-
+            h_pos[0] += md[dir][0]
+            h_pos[1] += md[dir][1]
             if abs(t_pos[0]-h_pos[0]) > 1 or abs(t_pos[1]-h_pos[1]) > 1:
+                xdis = 0 if h_pos[0] == t_pos[0] else (h_pos[0] - t_pos[0]) / abs(h_pos[0] - t_pos[0])
+                ydis = 0 if h_pos[1] == t_pos[1] else (h_pos[1] - t_pos[1]) / abs(h_pos[1] - t_pos[1])
+                t_pos[0] += int(xdis)
+                t_pos[1] += int(ydis)
+                t_set.add((t_pos[0],t_pos[1]))
 
-                t_pos = (t_pos + ((h_pos-t_pos)/2)).astype(np.int64)
-                t_list.append(t_pos)
+    return(len(t_set))
 
-    t_list = np.array(t_list)
-    vis_grid = np.zeros((np.max(t_list[:,0])+1, np.max(t_list[:,1])+1))
-    for coord in t_list:
-        vis_grid[coord[0],coord[1]] += 20
+def solution_part_two(input_path):
+    with open(input_path) as f:
+        lines = f.read()
+    lines = lines.split('\n')[:-1]
+    positions = [[0,0] for _ in range(10)]
+    t_set = set()
+    md = {'U':[0,1],'D':[0,-1],'R':[1,0],'L':[-1,0]}
 
-    final = np.zeros(np.shape(vis_grid)) + 1
-    final = np.where(vis_grid > 0, final, np.zeros(np.shape(vis_grid)))
-    print(np.sum(final))
-    vis_grid = cv2.resize(vis_grid, (500,500), interpolation = cv2.INTER_NEAREST)
-    cv2.imshow('window',vis_grid.astype(np.uint8))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    for line in lines:
+        dir, dis = line.split()
+        for i in range(int(dis)):
+            positions[-1][0] += md[dir][0]
+            positions[-1][1] += md[dir][1]
+            for x in range(8,-1,-1):
+                print('x is',x)
+                if abs(positions[x][0] - positions[x+1][0]) > 1 or abs(positions[x][1] - positions[x+1][1]) > 1:
+                    xdis = 0 if positions[x+1][0] == positions[x][0] else (positions[x+1][0] - positions[x][0]) / abs(positions[x+1][0] - positions[x][0])
+                    ydis = 0 if positions[x+1][1] == positions[x][1] else (positions[x+1][1] - positions[x][1]) / abs(positions[x+1][1] - positions[x][1])
+                    positions[x][0] += int(xdis)
+                    positions[x][1] += int(ydis)
+
+                if x == 0:
+                    print('here')
+                    t_set.add((positions[x][0], positions[x][1]))
 
 
-solution_part_one('input.txt')
+    return(len(t_set))
+
+
+print(solution_part_two('input.txt'))
