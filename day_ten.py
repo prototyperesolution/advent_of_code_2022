5fa4a39b79e6faa354d6cd415adff8a2077e391d
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-14 20:42:35 +0000

    commit

--- day_ten.py
+++ day_ten.py
@@ -24,5 +24,28 @@ def solution_part_one(input_path):
             sigstrength += int(instr.split()[-1])
     return ans
 
+def solution_part_two(input_path):
+    with open(input_path) as f:
+        lines = f.read()
+    instructions = lines.split('\n')[:-1]
+    cycles = 0
+    sigstrength = 1
+    ans = [['.' for x in range(40)]for i in range(6)]
+    for instr in instructions:
 
-print(solution_part_one('input.txt'))
+        if 'noop' in instr:
+            if cycles % 40 in range(sigstrength - 1, sigstrength + 2):
+                ans[cycles // 40][cycles % 40] = '#'
+            cycles += 1
+        else:
+            for i in range(2):
+                if cycles % 40 in range(sigstrength - 1, sigstrength + 2):
+                    ans[cycles // 40][cycles % 40] = '#'
+                cycles += 1
+            sigstrength += int(instr.split()[-1])
+
+
+    return ans
+
+for line in solution_part_two('input.txt'):
+    print(line)
