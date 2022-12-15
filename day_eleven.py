12ee14c16e928c88cbd645f67d24c21a4a0267e5
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-15 22:16:46 +0000

    commit

--- day_eleven.py
+++ day_eleven.py
@@ -4,7 +4,9 @@ os.chdir('D:/adventofcode2022/day eleven')
 import numpy as np
 import cv2
 
-class monkey:
+
+"""made two versions of the monkey class. There's deffo a more elegant way to do this """
+class monkey_part_one:
     def __init__(self,items, test, op, true, false):
         self.test = test
         self.op = op
@@ -28,7 +30,28 @@ class monkey:
             self.inspections += 1
         return pass_dict
 
+class monkey_part_two:
+    def __init__(self,items, test, op, true, false):
+        self.test = test
+        self.op = op
+        self.items = items
+        self.true = true
+        self.false = false
+        self.inspections = 0
+    def pass_to_monkey(self,item,target):
+        return item, target
 
+    def turn(self, monkeymod):
+        pass_dict = {self.true:[],self.false:[]}
+        while len(self.items) > 0:
+            curr = self.items.pop(0)
+            result = eval(''.join([str(curr),self.op])) % monkeymod
+            if result % self.test == 0:
+                pass_dict[self.true].append(result)
+            else:
+                pass_dict[self.false].append(result)
+            self.inspections += 1
+        return pass_dict
 
 
 
@@ -49,7 +72,7 @@ def solution_part_one(input_path):
             test = [int(x) for x in instr[3].split() if x.isdigit()][0]
             true, false = [[int(x) for x in instr[i].split() if x.isdigit()][0] for i in range(4,6)]
 
-            monkeys[name] = monkey(items, test, op, true, false)
+            monkeys[name] = monkey_part_one(items, test, op, true, false)
             i += 5
         i += 1
 
@@ -58,7 +81,38 @@ def solution_part_one(input_path):
         for j in monkeys.keys():
             pass_dict = monkeys[j].turn()
             for i in pass_dict.keys():
-                #print('monkey ',j,' passes to monkey ',i)
+                monkeys[i].items += pass_dict[i]
+
+
+    final = sorted([monkeys[x].inspections for x in monkeys])[::-1]
+    print(final[0]*final[1])
+
+def solution_part_two(input_path):
+    monkeys = {}
+    with open(input_path) as f:
+        lines = f.read()
+    lines = lines.split('\n')
+    i = 0
+    monkeymod = 1
+    """parsing"""
+    while i < len(lines)-6:
+        if 'Monkey' in lines[i]:
+            instr = lines[i:i+6]
+            name = [int(x) for x in instr[0] if x.isdigit()][0]
+            items = [int(x) for x in instr[1][17:].split(',')]
+            op = '**2' if 'old' in instr[2].split()[-1] else ''.join(instr[2].split()[len(instr[2].split())-2:len(instr[2].split())+1])
+            test = [int(x) for x in instr[3].split() if x.isdigit()][0]
+            true, false = [[int(x) for x in instr[i].split() if x.isdigit()][0] for i in range(4,6)]
+            monkeymod *= test
+            monkeys[name] = monkey_part_two(items, test, op, true, false)
+            i += 5
+        i += 1
+
+    """main logic"""
+    for rounds in range(10000):
+        for j in monkeys.keys():
+            pass_dict = monkeys[j].turn(monkeymod)
+            for i in pass_dict.keys():
                 monkeys[i].items += pass_dict[i]
 
 
@@ -66,5 +120,4 @@ def solution_part_one(input_path):
     print(final[0]*final[1])
 
 
-
-solution_part_one('input.txt')
+solution_part_two('input.txt')
