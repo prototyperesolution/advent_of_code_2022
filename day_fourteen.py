0d76cbdf1f39fd9155c1754cefceef0e1ea1230f
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-22 22:07:37 +0000

    commit

--- day_fourteen.py
+++ day_fourteen.py
@@ -11,19 +11,23 @@ def parse_input(input_path):
     with open(input_path) as f:
         lines = f.read()
     lines= lines.split('\n')[:-1]
-    grid = np.zeros((600,200))
+    '''setting grid to be arbitrarily large, this is ugly'''
+    grid = np.zeros((1000,200))
+    total_coords = []
     for line in lines:
         curr = re.findall(r'\d+', ''.join(line))
         curr = [int(x) for x in curr]
         curr = np.reshape(np.array(curr), (len(curr)//2,2))
         for i in range(len(curr) - 1):
+            total_coords.append(curr[i])
             grid[curr[i][0], curr[i][1]] = 1
             if curr[i + 1][0] == curr[i][0]:
                 grid[curr[i][0], np.min([curr[i][1], curr[i+1][1]]):np.max([curr[i][1], curr[i+1][1]])+1] = 1
             if curr[i + 1][1] == curr[i][1]:
                 grid[np.min([curr[i][0],curr[i + 1][0]]):np.max([curr[i][0], curr[i+1][0]])+1, curr[i][1]] = 1
-
-    return(grid)
+        total_coords.append(curr[-1])
+    max_depth = np.max(np.array(total_coords)[:,1])
+    return(grid, max_depth)
 
 
 def drop_sand(source, grid):
@@ -47,7 +51,24 @@ def drop_sand(source, grid):
 
 
 def solution_part_one(input_path):
-    grid = parse_input(input_path)
+    grid, _ = parse_input(input_path)
+    source = np.array([500,0])
+    while True:
+        new = drop_sand(source, grid)
+        if new.all() != source.all():
+            grid[new[0],new[1]] = 2
+        else:
+            break
+    grid = grid//2
+    total = np.sum(grid)
+    print(total)
+    cv2.imshow('grid', grid)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def solution_part_two(input_path):
+    grid, max_depth = parse_input(input_path)
+    grid[:,max_depth+2] = 1
     source = np.array([500,0])
     while True:
         new = drop_sand(source, grid)
@@ -64,4 +85,4 @@ def solution_part_one(input_path):
 
 
 
-solution_part_one('input.txt')
+solution_part_two('input.txt')
